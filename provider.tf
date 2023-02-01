042c5344e71dfda1d79ea0437400628253e35e09
Bruvv <nvoorn@gmail.com>
2023-02-01 12:23:56 +0100

    move to private key

--- provider.tf
+++ provider.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  private_key_path = var.private_key_path
-  fingerprint      = var.fingerprint
-  region           = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  private_key  = var.private_key
+  fingerprint  = var.fingerprint
+  region       = var.region
 }
