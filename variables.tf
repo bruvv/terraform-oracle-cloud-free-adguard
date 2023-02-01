06f053abce5a4a74268060cb68c61a2a27c3a87e
Bruvv <nvoorn@gmail.com>
2023-02-01 11:08:01 +0100

    fix variables explaining

--- variables.tf
+++ variables.tf
@@ -15,15 +15,15 @@ variable "instance_shape_config_memory_in_gbs" {
   default = 23 #ARM = 24, AMD = 1
 }
 
-variable "tenancy_ocid" {
+variable "tenancy_ocid" { # https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#five
   default = "ocid1.tenancy.oc1..CHANGETHIS"
 }
 
-variable "user_ocid" {
+variable "user_ocid" { # https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#five
   default = "ocid1.user.oc1..CHANGETHIS"
 }
 
-variable "fingerprint" {
+variable "fingerprint" { # https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#four
   default = "11:22:33.........."
 }
 
@@ -53,10 +53,7 @@ variable "flex_instance_image_ocid" {
   # See https://docs.us-phoenix-1.oraclecloud.com/images/
   type = map(string)
   default = {
-    eu-amsterdam-1 = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaadw6rseqgm33aytx35duudrm6edzp5lhhvutmkxzt45kn4szreraa"
-    # us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaa6hooptnlbfwr5lwemqjbu3uqidntrlhnt45yihfj222zahe7p3wq"
-    # us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaa6tp7lhyrcokdtf7vrbmxyp2pctgg4uxvt4jz4vc47qoc2ec4anha"
-    # eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaadvi77prh3vjijhwe5xbd6kjg3n5ndxjcpod6om6qaiqeu3csof7a"
-    # uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaaw5gvriwzjhzt2tnylrfnpanz5ndztyrv3zpwhlzxdbkqsjfkwxaq"
+    # eu-amsterdam-1 = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaadw6rseqgm33aytx35duudrm6edzp5lhhvutmkxzt45kn4szreraa" # ubuntu
+    eu-amsterdam-1 = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaa526auzq66wvw4m2f4l2jhvlluvjc6njpnzqjtuiq23ghb3u5ymoa" # oracle 9
   }
 }
